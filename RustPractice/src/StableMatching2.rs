2c8ef9075aa1a80b113fc43a877094cfc9ac38e4
Six square <99231417+Imonanoko@users.noreply.github.com>
2023-07-05 21:14:07 +0800

    0705註解更新

--- RustPractice/src/StableMatching2.rs
+++ RustPractice/src/StableMatching2.rs
@@ -45,31 +45,46 @@ pub fn main(){
     let mut women_status:[bool;5] = [false;5];
     //與哪位男生配對
     let mut matching_with_man:[i32;5] = [-1;5];
+    //檢查是否還有單身男子
     while !men_status.is_empty() {
+        //現在是誰追求，因為pop可能跳出空值所以要預防空值發生。用.unwrap_or(-1)，如果為空值則傳-1出去
         let who:i32 = men_status.pop_back().unwrap_or(-1);
         if who == -1 {
             println!("Error");
             break;
         }
+        //男子追求的路上
         loop {
+            //目前追求的女孩
             let girl_friend:i32=men[who as usize][men_choice[who as usize] as usize];
+            //如果單身
             if !women_status[girl_friend as usize] {
+                //將狀態改成現充
                 women_status[girl_friend as usize]=true;
+                //如果被分手下次從哪個排名找
                 men_choice[who as usize]+=1;
+                //紀錄女孩跟誰交往
                 matching_with_man[girl_friend as usize]=who;
                 break;
             }
+            //如果有男友
             else{
+                //女孩現任男友
                 let cur:i32=matching_with_man[girl_friend as usize];
+                //如果現任排名比追求者差的話
                 if Ranking_of_men_among_women[girl_friend as usize][who as usize]<Ranking_of_men_among_women[girl_friend as usize][cur as usize] {
+                    //將現任設定為單身
                     men_status.push_back(cur);
+                    //更新女孩交往紀錄
                     matching_with_man[girl_friend as usize]=who;
                     break;
                 }
+                //如果被分手下次從哪個排名找
                 men_choice[who as usize]+=1;
             }
         }
     }
+    //輸出誰跟誰在一起，前面男生後面女生
     for i in 0..matching_with_man.len() {
         println!("{}:{}",matching_with_man[i],i);
     }
