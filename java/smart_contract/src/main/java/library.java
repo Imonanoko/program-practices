15a62cd48b23638ff8502e3d98b1bc8f9ad97b47
Six square <99231417+Imonanoko@users.noreply.github.com>
2023-08-02 11:37:39 +0800

    20230802 variable analyses

--- java/smart_contract/src/main/java/library.java
+++ java/smart_contract/src/main/java/library.java
@@ -1,11 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
 //"C:\\Users\\Six square\\Desktop\\程式語言練習\\java\\smart_contract\\src\\main\\java\\contract.txt"
 public class library {
-    protected static List<List<String>> file_analyses (String Path) throws FileNotFoundException {
+    protected static List<List<String>> file_analyses(String Path) throws FileNotFoundException {
         List<List<String>> lines = new ArrayList<>();
         File doc = new File(Path);
         Scanner obj = new Scanner(doc);
@@ -30,22 +29,63 @@ public class library {
                             word.setLength(0);
                         }
                     }
+                    //將字串單獨放入一個格子裡，因為如果遇到特殊字元會被切割
+                    case '\"' -> {
+                        if (!word.toString().equals("")) {
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                        }
+                        word_List.add(Character.toString(chr));
+                        i++;
+                        char tmp = str.charAt(i);
+                        while (tmp != '\"') {
+                            word.append(tmp);
+                            i++;
+                            tmp = str.charAt(i);
+                        }
+                        if (!word.toString().equals("")) {
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                        }
+                        word_List.add(Character.toString(chr));
+                        i++;
+                    }
                     //如果是;、(、)等字元會被區分開來，亦即;、(、{是被獨立加進word裡面的
-                    case ';', '(', ')', '{', '}' -> {
-                        word_List.add(word.toString());
-                        word.setLength(0);
+                    case ';', '(', ')', '{', '}', '.', '\'', ',' -> {
+                        if (!word.toString().equals("")) {
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                        }
                         word_List.add(Character.toString(chr));
                     }
                     //因為=、<、>可能後面接一樣=、<、>的字元所以需要檢查後面是否有這些字元，沒有就加進word裡面、有就把兩個字元一起加進去word
-                    case '=', '<', '>' -> {
+                    case '=', '<', '>', '|', '&', '!', '*' -> {
                         if (!word.toString().equals("")) {
                             word_List.add(word.toString());
                             word.setLength(0);
                         }
                         char tmp = str.charAt(i + 1);
-                        if (tmp == '=' || tmp == '>' || tmp == '<') {
+                        switch (tmp) {
+                            case '=', '<', '>', '|', '&', '!', '*' -> {
+                                word.append(chr);
+                                word.append(tmp);
+                                word_List.add(word.toString());
+                                word.setLength(0);
+                                i++;
+                            }
+                            default -> word_List.add(Character.toString(chr));
+                        }
+
+                    }
+                    //遇到運算式需要將符號分開並檢查後面是否有符號
+                    case '+', '-', '%' -> {
+                        if (!word.toString().equals("")) {
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                        }
+                        if (str.charAt(i + 1) == '=') {
                             word.append(chr);
-                            word.append(tmp);
+                            word.append(str.charAt(i + 1));
                             word_List.add(word.toString());
                             word.setLength(0);
                             i++;
@@ -57,19 +97,138 @@ public class library {
                         if (str.charAt(i + 1) == '/') {
                             break out;
                         }
+                        if (!word.toString().equals("")) {
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                        }
+                        if (str.charAt(i + 1) == '=') {
+                            word.append(chr);
+                            word.append(str.charAt(i + 1));
+                            word_List.add(word.toString());
+                            word.setLength(0);
+                            i++;
+                        } else {
+                            word_List.add(Character.toString(chr));
+                        }
                     }
                     //非上述字串就會當成跟前面的字元是一組的
-                    default -> {
-                        word.append(chr);
-                    }
+                    default -> word.append(chr);
                 }
+            }
+            if (!word.toString().equals("")) {
+                word_List.add(word.toString());
             }
             lines.add(new ArrayList<>(word_List));
         }
         return lines;
     }
-    //輸出格是每一列表一個問題[第幾行的if or require,甚麼變數,可能出現什麼值造成錯誤];
-    protected static void logic_test(List<List<String>> lines){
 
+    //輸出格是每一列表一個問題[第幾行的if or require,甚麼變數,可能出現什麼值造成錯誤];
+    //輸入:
+    //lines:檔案分析後的資料
+    protected static List<List<List<String>>> variable_analyses(List<List<String>> lines) {
+        List<List<List<String>>> var = new ArrayList<>();
+        //存取全域變數的名稱
+        List<List<String>> contract_global_var = new ArrayList<>();
+        //不同合約有不同的全域變數[合約名稱,變數名稱]
+        List<String> global_var = new ArrayList<>();
+        //存取區域變，每個row是[函數名稱,裡面的變數名稱]
+        List<List<String>> function_local_var = new ArrayList<>();
+        //function_local_var 的row暫存
+        List<String> local_var = new ArrayList<>();
+        //判斷大括號的暫存器[contract,function,if_elif,for,while]
+        List<Integer> flag = new ArrayList<>();
+        String word;
+        // i+1為行數
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = 0; j < lines.get(i).size(); j++) {
+                word = lines.get(i).get(j);
+                switch (word) {
+                    case "contract" ->{
+                        flag.add(0);
+                        //前面有合約就加入到contract_global_var
+                        if(global_var.size()!=0){
+                            contract_global_var.add(new ArrayList<>(global_var));
+                            global_var.clear();
+                        }
+                        //加入名稱
+                        j++;
+                        global_var.add(lines.get(i).get(j));
+                    }
+                    case "function" ->{
+                        flag.add(0);
+                        if(local_var.size()!=0){
+                            function_local_var.add(new ArrayList<>(local_var));
+                            local_var.clear();
+                        }
+                        j++;
+                        local_var.add(lines.get(i).get(j));
+                    }
+                    case  "if" -> {
+                        flag.add(0);
+                    }
+                    case "else" -> {
+                        flag.add(0);
+                        if (lines.get(i).get(j + 1).equals("if"))
+                            j++;
+                    }
+                    case "{" -> {
+                        int index = flag.size()-1;
+                        //旗標+1
+                        flag.set(index, flag.get(index) + 1);
+                    }
+                    case "}" -> {
+                        int index = flag.size()-1;
+                        //旗標-1
+                        flag.set(index,flag.get(index) - 1);
+                        if(flag.get(index)==0){
+                            flag.remove(index);
+                        }
+                    }
+                    case "uint","address","uint256" ->{
+                        j++;
+                        word = lines.get(i).get(j);
+                        switch (word){
+                            //待新增，可能遇到宣告型別是陣列之類的
+                            case ")","("->{
+
+                            }
+                            default -> {
+                                //等於一代表全域變數
+                                if(flag.size()==1){
+
+                                    switch (word){
+                                        case "public","private" ->{
+                                            j++;
+                                            global_var.add(lines.get(i).get(j));
+                                        }
+                                        default -> {
+                                            global_var.add(lines.get(i).get(j));
+                                        }
+                                    }
+                                }
+                                else{
+                                    local_var.add(word);
+                                }
+                            }
+                        }
+
+                    }
+                }
+//                switch (word){
+//                    case "if" ->{
+//                        System.out.println("if at:"+(i+1)+":"+j);
+//                    }
+//                    case "require" ->{
+//                        System.out.println("require at:"+(i+1)+":"+j);
+//                    }
+//                }
+            }
+        }
+        contract_global_var.add(new ArrayList<>(global_var));
+        function_local_var.add(new ArrayList<>(local_var));
+        var.add(new ArrayList<>(contract_global_var));
+        var.add(new ArrayList<>(function_local_var));
+        return var;
     }
 }
--- java/smart_contract/src/main/java/main.java
+++ java/smart_contract/src/main/java/main.java
@@ -8,8 +8,21 @@ public class main {
     public static void main(String[] args) throws FileNotFoundException {
         String path = "C:\\Users\\Six square\\Desktop\\µ{¦¡»y¨¥½m²ß\\java\\smart_contract\\src\\main\\java\\contract.txt";
         List<List<String>> line = library.file_analyses(path);
-        for (List<String> str : line) {
+//        System.out.println(line.size());
+        for(List<String> str : line){
             System.out.println(str);
+        }
+
+
+
+        List<List<List<String>>> outcome = library.variable_analyses(line);
+        List<List<String>> contract_global_var = new ArrayList<>(outcome.get(0));
+        List<List<String>> function_local_var = new ArrayList<>(outcome.get(1));
+        for (List<String> global_var : contract_global_var) {
+            System.out.println(global_var);
+        }
+        for (List<String> local_var : function_local_var) {
+            System.out.println(local_var);
         }
 
     }
